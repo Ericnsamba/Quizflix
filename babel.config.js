ae46151b655c1c1dc49a827e268c3373bfdfb18e
Ericnsamba <Eric.nsamba@kurtosys.com>
2020-07-04 14:51:39 +0200

    type

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+	presets: ['module:metro-react-native-babel-preset', '@babel/typescript'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['./'],
+				extensions: ['.js', '.ts', '.tsx', '.ios.js', '.android.js'],
+			},
+		],
+		'@babel/plugin-transform-runtime',
+	],
 };
